77bdddb755c5c5e65df9986fccb4a6c0976c0eae
Loïc Steinmetz <loic.steinmetz2@gmail.com>
2020-04-06 22:05:55 +0200

    ajoute la configuration de la bdd dev/prod

--- app/src/main/java/com/pleiade/android/MainActivity.java
+++ app/src/main/java/com/pleiade/android/MainActivity.java
@@ -3,14 +3,44 @@ package com.pleiade.android;
 import android.os.Bundle;
 import android.util.Log;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.pleiade.android.utils.DatabaseManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        DatabaseManager.initialize();
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i("FirebaseEmulator", String.valueOf(BuildConfig.FIREBASE_EMULATOR));
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        DocumentReference docRef = db.collection("users").document("test");
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    assert document != null;
+                    if (document.exists()) {
+                        Log.i("TEST_DB", "Data: " + document.getData());
+                    } else {
+                        Log.i("TEST_DB", "Pas de document 'users'");
+                    }
+                } else {
+                    Log.i("TEST_DB", "Erreur de lecture");
+                }
+            }
+        });
+
     }
 }
