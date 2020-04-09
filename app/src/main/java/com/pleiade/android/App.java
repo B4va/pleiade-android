930b250856a0e5da231931635343f6b66cb9961a
Loïc Steinmetz <loic.steinmetz2@gmail.com>
2020-04-09 23:41:47 +0200

    ajoute doc

--- app/src/main/java/com/pleiade/android/App.java
+++ app/src/main/java/com/pleiade/android/App.java
@@ -2,8 +2,14 @@ package com.pleiade.android;
 
 import android.app.Application;
 
+/**
+ * Classe exécutée au lancement de l'application
+ */
 public class App extends Application {
 
+    /**
+     * Exécutée à la création de l'instance
+     */
     @Override
     public void onCreate() {
         super.onCreate();
