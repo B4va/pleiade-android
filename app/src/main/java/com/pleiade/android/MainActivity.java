4e7ed4075ea35ad592a266bda36eade84bf4d2d4
Loïc Steinmetz <loic.steinmetz2@gmail.com>
2020-04-09 10:19:36 +0200

    ajoute le splash screen

--- app/src/main/java/com/pleiade/android/MainActivity.java
+++ app/src/main/java/com/pleiade/android/MainActivity.java
@@ -1,50 +1,13 @@
 package com.pleiade.android;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.FirebaseFirestore;
-
-import java.util.Objects;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        /* Teste la récupération de données test
-           'bava_prod' pour la BDD de production
-           'bava_dev' pour la BDD de développement
-         */
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-        DocumentReference docRef = db.collection("users").document("test");
-        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                final String TAG = "DbTests";
-                if (task.isSuccessful()) {
-                    DocumentSnapshot document = task.getResult();
-                    assert document != null;
-                    if (document.exists()) {
-                        Log.i(TAG, "Data: " + document.getString("name"));
-                    } else {
-                        Log.e(TAG, "Pas de document 'users'");
-                    }
-                } else {
-                    Log.e(TAG, "Erreur de lecture");
-                }
-            }
-        });
-
     }
 }
