05a570725b53dc2443d8c35d1ae1cdd9886e165e
Loïc Steinmetz <loic.steinmetz2@gmail.com>
2020-04-10 08:30:50 +0200

    ajoute la redirection vers MainActivity

--- app/src/main/java/com/pleiade/android/activities/SplashActivity.java
+++ app/src/main/java/com/pleiade/android/activities/SplashActivity.java
@@ -1,7 +1,9 @@
 package com.pleiade.android.activities;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.pleiade.android.R;
 
@@ -9,6 +11,9 @@ import com.pleiade.android.R;
  * Ecran de chargement au lancement de l'activité
  */
 public class SplashActivity extends Activity {
+
+
+    private static final int TRANSITION_TIMEOUT = 3000;
 
     /**
      * Exécutée à la création de l'instance
@@ -18,5 +23,15 @@ public class SplashActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        /* Redirige vers MainActivity */
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, TRANSITION_TIMEOUT);
     }
 }
