57ed2a8a53adc467fcfa8f9ccc2f6df8484d6e02
Loïc Steinmetz <loic.steinmetz2@gmail.com>
2020-04-13 19:09:35 +0200

    configure les tests bdd (! - non fonctionnel)

--- app/src/test/java/com/pleiade/android/UserDbTest.java
+++ app/src/test/java/com/pleiade/android/UserDbTest.java
@@ -1,20 +1,50 @@
 package com.pleiade.android;
 
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.api.ContextOrBuilder;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreSettings;
 import com.pleiade.android.models.User;
 
-import org.junit.Test;
-
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 /**
  * Tests du modèle User
  * @see User
  */
-public class UserTest {
+public class UserDbTest {
 
-    /**
-     * Tests du modèle en base de données et des règles de sécurité
-     */
+    private static FirebaseFirestore db;
+    private static FirebaseUser user;
+
+    @BeforeAll
+    public static void dbEmulatorInitialisation() {
+        // todo
+//        FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder()
+//                .setHost("http://localhost:8080")
+//                .setSslEnabled(false)
+//                .setPersistenceEnabled(false)
+//                .build();
+//
+//        FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+//        firestore.setFirestoreSettings(settings);
+//        db = firestore;
+    }
+
+    @BeforeAll
+    public static void authenticatedUserIntitialisation(){
+        // todo
+    }
 
     @Test
     public void validUserCreation() {
@@ -72,7 +102,29 @@ public class UserTest {
 
     @Test
     public void invalidUserUpdate() {
+        // sans authentification
 
+        // sans appartenance à l'utilisateur authentifié
+
+        // sans prénom
+
+        // sans nom
+
+        // sans tag
+
+        // avec date de création
+
+        // sans date de modification
+
+        // avec un champ non existant
+
+        // avec un prénom non conforme
+
+        // avec un nom non conforme
+
+        // avec un email non conforme
+
+        // avec un tag non conforme
     }
 
     @Test
@@ -82,20 +134,21 @@ public class UserTest {
 
     @Test
     public void invalidUserDeletion() {
+        // sans authentification
 
+        // sans appartenance à l'utilisateur authentifié
     }
 
     @Test
     public void validUserRead() {
+        // avec appartenance à l'utilisateur authentifié
 
+        // sans appartenance à l'utilisateur authentifié
     }
 
     @Test
     public void invalidUserRead() {
+        // sans authentification
 
     }
-
-    /**
-     * Tests des méthodes objet
-     */
 }
