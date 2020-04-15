d4f198c47e5c14c7b6e4366c83ec3efd8c7c2363
Loïc Steinmetz <loic.steinmetz2@gmail.com>
2020-04-15 13:10:53 +0200

    réinitialise la configuration bdd/modèle

---
+++ app/src/main/java/com/pleiade/android/models/Model.java
@@ -0,0 +1,59 @@
+package com.pleiade.android.models;
+
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+
+import java.util.Map;
+
+public abstract class Model {
+
+    private DocumentReference ref;
+
+    /**
+     * Constructeur vide, pour création du modèles
+     */
+    public Model(){ }
+
+    /**
+     * Constructeur référencé, pour lecture, mise à jour
+     * et suppression du modèle
+     * @param ref référence Firestore
+     */
+    public Model(DocumentReference ref){
+        this.ref = ref;
+    }
+
+    /**
+     * Crée le modèle
+     * @param modelMap champs et valeurs
+     * @return tâche de création du modèle
+     */
+    public abstract Task create(Map<String, Object> modelMap);
+
+    /**
+     * Accède aux données du modèles
+     * @return tâche d'accès aux données
+     */
+    public abstract Task read();
+
+    /**
+     * Met à jour le modèle
+     * @param modelMap champs et valeurs
+     * @return tâche de mise à jour du modèle
+     */
+    public abstract Task update(Map<String, Object> modelMap);
+
+    /**
+     * Supprime le modèle
+     * @return tâche de suppression du modèle
+     */
+    public abstract Task delete();
+
+    /**
+     * Retourne la référence Firestore du modèle
+     * @return référence Firestore du modèle
+     */
+    public DocumentReference getRef(){
+        return  ref;
+    }
+}
