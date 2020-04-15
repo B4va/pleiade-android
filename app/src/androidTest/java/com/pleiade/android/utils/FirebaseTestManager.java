9065d1e9a99ab4c4c9116d84da130f8a0c4bfb57
Loïc Steinmetz <loic.steinmetz2@gmail.com>
2020-04-16 01:21:06 +0200

    entamme limplementation des tests User

--- app/src/androidTest/java/com/pleiade/android/utils/FirebaseTestManager.java
+++ app/src/androidTest/java/com/pleiade/android/utils/FirebaseTestManager.java
@@ -1,55 +1,82 @@
 package com.pleiade.android.utils;
 
 import android.content.Context;
+import android.net.Uri;
 
-import androidx.annotation.NonNull;
 import androidx.test.core.app.ApplicationProvider;
 
-import com.google.android.gms.tasks.Continuation;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.Task;
 import com.google.android.gms.tasks.Tasks;
 import com.google.firebase.FirebaseApp;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreSettings;
 
 import java.util.Objects;
-import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
-import static org.junit.Assume.assumeNotNull;
-import static org.junit.Assume.assumeTrue;
-
+/**
+ * Classe utilitaire d'initialisation et de paramétrage
+ * des instances Firebase
+ */
 public class FirebaseTestManager {
 
     private static final int AUTH_TIMEOUT = 3;
+    public static final String USER1_FIRST_NAME = "Charlotte";
+    public static final String USER2_FIRST_NAME = "Jean";
+    public static final String USER1_LAST_NAME = "Corday";
+    public static final String USER2_LAST_NAME = "Jaurès";
     public static final String USER1_EMAIL = "user1@mail.com";
     public static final String USER2_EMAIL = "user2@mail.com";
     public static final String USER_PASSWORD = "123456";
+    public static final Uri USER_PHOTO_URI = Uri.parse("test/test/test");
+    public static final String USER1_DISPLAY_NAME = "user1";
+    public static final String USER2_DISPLAY_NAME = "user2";
 
+    /**
+     * Initialise l'application Firebase
+     */
     public static void initializeFirebaseApp(){
         Context context = ApplicationProvider.getApplicationContext();
         FirebaseApp.initializeApp(context);
     }
 
+    /**
+     * Initialise l'émulateur FirebaseFirestore
+     * @return instance de l'émulateur
+     */
     public static FirebaseFirestore initializeFirebaseFirestoreEmulator() {
         FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder()
                 .setHost("localhost:8080")
                 .setSslEnabled(false)
-                .setPersistenceEnabled(false)
+                .setPersistenceEnabled(true)
                 .build();
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         db.setFirestoreSettings(settings);
         return db;
     }
 
+    /**
+     * Ecrase les données du simulateur FirebaseFirestore
+     */
+    public static void clearFirebaseFirestoreEmulator() {
+        FirebaseFirestore.getInstance().clearPersistence();
+    }
+
+    /**
+     * Initialise FirebaseAuth
+     * @return instance FirebaseAuth
+     */
     public static FirebaseAuth initializeFirebaseAuth(){
         return FirebaseAuth.getInstance();
     }
 
+    /**
+     * Connecte un utilisateur
+     * @param auth instance FirebaseAuth
+     * @param email email de l'utilisateur
+     * @param password mot de passe de l'utilisateur
+     * @throws Exception si tâche interrompue
+     */
     public static void firebaseAuthLogin(FirebaseAuth auth, String email, String password) throws Exception {
         if (auth.getCurrentUser() == null){
             Tasks.await(auth.signInWithEmailAndPassword(email, password), AUTH_TIMEOUT, TimeUnit.SECONDS);
@@ -64,6 +91,10 @@ public class FirebaseTestManager {
         }
     }
 
+    /**
+     * Déconnecte l'utilisateur authentifié
+     * @param auth instance FirebaseAuth
+     */
     public static void firebaseAuthLogout(FirebaseAuth auth){
         if (auth.getCurrentUser() != null){
             auth.signOut();
--- app/src/main/java/com/pleiade/android/models/User.java
+++ app/src/main/java/com/pleiade/android/models/User.java
@@ -1,20 +1,38 @@
 package com.pleiade.android.models;
 
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.Timestamp;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.Map;
+import java.util.Objects;
 
 /**
  * Modèlisation d'un utilisateur
  */
 public class User extends Model {
 
+    private static final String TAG = "UserModel";
     private String firstName, lastName, email, tag, profilePictureUri;
     private Timestamp createdAt, modifiedAt;
+
+    /**
+     * Constructeur vide, pour création
+     */
+    public User(){}
+
+    /**
+     * Constructeur avec référence pour lecture,
+     * mise à jour et suppression
+     * @param ref référence de l'utilisateur en base de données
+     */
+    public User(DocumentReference ref){
+        this.ref = ref;
+    }
 
     /**
      * Crée un utilisateur
@@ -23,18 +41,25 @@ public class User extends Model {
      */
     @Override
     public Task create(Map<String, Object> modelMap) {
-        // todo : création du compte et connexion FireAuth ; cf transactions
-        modelMap.put("email", FirebaseAuth.getInstance().getCurrentUser().getEmail());
-        this.ref = FirebaseFirestore.getInstance()
-                .collection("users")
-                .document(FirebaseAuth.getInstance().getCurrentUser().getUid());
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        ref = FirebaseFirestore.getInstance()
+            .collection("users")
+            .document(Objects.requireNonNull(user).getUid());
+
+        modelMap.put("email", user.getEmail());
+        modelMap.put("tag", user.getDisplayName());
+        modelMap.put("profilePictureUri", Objects.requireNonNull(user.getPhotoUrl()).toString());
+        addCreatedTimestamps(modelMap);
+
         Task<Void> t = ref.set(modelMap);
         t.addOnSuccessListener(aVoid -> {
             firstName = (String) modelMap.get("firstName");
             lastName = (String) modelMap.get("lastName");
-            email = FirebaseAuth.getInstance().getCurrentUser().getEmail();
+            email = (String) modelMap.get("email");
             tag = (String) modelMap.get("tag");
             profilePictureUri = (String) modelMap.get("profilePictureUri");
+            createdAt = (Timestamp) modelMap.get("createdAt");
+            modifiedAt = (Timestamp) modelMap.get("modifiedAt");
         });
         return t;
     }
@@ -44,8 +69,18 @@ public class User extends Model {
      * @return tâche d'acès aux données de l'utilisateur
      */
     @Override
-    public Task read() {
-        return null;
+    public Task<DocumentSnapshot> read() {
+        Task<DocumentSnapshot> t = ref.get();
+        t.addOnSuccessListener(documentSnapshot -> {
+            firstName = (String) documentSnapshot.get("firstName");
+            lastName = (String) documentSnapshot.get("lastName");
+            email = (String) documentSnapshot.get("email");
+            tag = (String) documentSnapshot.get("tag");
+            profilePictureUri = (String) documentSnapshot.get("profilePictureUri");
+            createdAt = (Timestamp) documentSnapshot.get("createdAt");
+            modifiedAt = (Timestamp) documentSnapshot.get("modifiedAt");
+        });
+        return t;
     }
 
     /**
@@ -55,7 +90,14 @@ public class User extends Model {
      */
     @Override
     public Task update(Map<String, Object> modelMap) {
-        return null;
+        addModifiedTimestamp(modelMap);
+        Task<Void> t = ref.update(modelMap);
+        t.addOnSuccessListener(aVoid -> {
+            if (modelMap.get("firstName") != null) firstName = (String) modelMap.get("firstName");
+            if (modelMap.get("lastName") != null)lastName = (String) modelMap.get("lastName");
+            modifiedAt = (Timestamp) modelMap.get("modifiedAt");
+        });
+        return t;
     }
 
     /**
