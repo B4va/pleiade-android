34b9c6085c20cd55f41e99961a8f27268b3836a3
Loïc Steinmetz <loic.steinmetz2@gmail.com>
2020-04-20 15:26:44 +0200

    renomme Test > ValidationMethod pour eviter la confusion avec classes de test

--- app/src/main/java/com/pleiade/android/utils/ValidationMethod.java
+++ app/src/main/java/com/pleiade/android/utils/ValidationMethod.java
@@ -4,7 +4,7 @@ package com.pleiade.android.utils;
  * Permet la création de méthodes de test à la volée
  * @param <Map> données à tester
  */
-public interface Test<Map> {
+public interface ValidationMethod<Map> {
 
     /**
      * Définit la méthode de test
