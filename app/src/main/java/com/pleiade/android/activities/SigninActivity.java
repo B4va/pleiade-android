af9b50c72a027258a65f84a14016c049e7915350
Loïc Steinmetz <loic.steinmetz2@gmail.com>
2020-04-22 09:03:07 +0200

    ajustements front

--- app/src/main/java/com/pleiade/android/activities/SigninActivity.java
+++ app/src/main/java/com/pleiade/android/activities/SigninActivity.java
@@ -5,12 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.pleiade.android.R;
+import com.pleiade.android.utils.UIHelper;
 
 public class SigninActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        UIHelper.setActionBar(this);
         setContentView(R.layout.activity_signin);
     }
 }
