44442e1ad97a6c4a11dd2e66fa6cf87a474f3b94
Loïc Steinmetz <loic.steinmetz2@gmail.com>
2020-04-22 10:41:43 +0200

    ajoute la vue de connexion

--- app/src/main/java/com/pleiade/android/activities/SigninActivity.java
+++ app/src/main/java/com/pleiade/android/activities/SigninActivity.java
@@ -12,7 +12,6 @@ public class SigninActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        UIHelper.setActionBar(this);
         setContentView(R.layout.activity_signin);
     }
 }
