9020ce291d8c4912ee4b4331c42e7b319682438b
Loïc Steinmetz <loic.steinmetz2@gmail.com>
2020-04-22 10:55:57 +0200

    ajoute la documentation

--- app/src/main/java/com/pleiade/android/activities/SigninActivity.java
+++ app/src/main/java/com/pleiade/android/activities/SigninActivity.java
@@ -7,8 +7,15 @@ import android.os.Bundle;
 import com.pleiade.android.R;
 import com.pleiade.android.utils.UIHelper;
 
+/**
+ * Vue d'inscription
+ */
 public class SigninActivity extends AppCompatActivity {
 
+    /**
+     * Exécutée à la création de l'instance
+     * @param savedInstanceState instance sauvegardée
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
